55fafe306e0b11feae1b418dc45d55ef18b67c3a
cgombauld <clement.gombauld@cpexterne.org>
2025-05-08 17:35:03 +0200

    refactor(Pharmacy): partion start to refacto with template pattern

--- pharmacy.js
+++ pharmacy.js
@@ -6,41 +6,82 @@ export class Drug {
   }
 
   updateDaily() {
-    // handle expiration update
-    if (this.name != "Magic Pill") {
-      this.expiresIn = this.expiresIn - 1;
-    }
-    // handle benefit update
-    if (this.name === "Herbal Tea") {
-      if (this.benefit < 50) {
-        this.benefit = this.benefit + 1;
-      }
-    } else if (this.name === "Fervex") {
-      if (this.benefit < 50) {
-        this.benefit = this.benefit + 1;
-        if (this.expiresIn < 10) {
-          this.benefit = this.benefit + 1;
-        }
-        if (this.expiresIn < 5) {
-          this.benefit = this.benefit + 1;
-        }
-      }
-    } else if (this.name === "Magic Pill") {
+    let drug;
+    if (this.name == "Magic Pill") {
+      drug = new MagicPillTemplate(this.expiresIn, this.benefit);
+    } else if (this.name == "Fervex") {
+      drug = new FervexTemplate(this.expiresIn, this.benefit);
+    } else if (this.name == "Herbal Tea") {
+      drug = new HerbalTeaTemplate(this.expiresIn, this.benefit);
     } else {
-      if (this.benefit > 0) {
-        this.benefit = this.benefit - 1;
-      }
+      drug = new DrugTemplate(this.expiresIn, this.benefit);
     }
+    drug.updateExpiresIn();
+    drug.updateBenefit();
+    drug.updateBenefitWhenExpired();
 
-    // handle benefit update when expires
+    this.expiresIn = drug.expiresIn;
+    this.benefit = drug.benefit;
+  }
+}
+
+class DrugTemplate {
+  constructor(expiresIn, benefit) {
+    this.expiresIn = expiresIn;
+    this.benefit = benefit;
+  }
+
+  updateExpiresIn() {
+    this.expiresIn = this.expiresIn - 1;
+  }
+  updateBenefit() {
+    if (this.benefit > 0) {
+      this.benefit = this.benefit - 1;
+    }
+  }
+
+  updateBenefitWhenExpired() {
     if (this.expiresIn < 0) {
-      if (this.name === "Fervex") {
-        this.benefit = 0;
-      } else if (this.name === "Herbal Tea") {
-        this.benefit = this.benefit + 1;
-      } else {
-        this.benefit = this.benefit - 1;
-      }
+      this.benefit = this.benefit - 1;
+    }
+  }
+}
+
+class HerbalTeaTemplate extends DrugTemplate {
+  updateBenefit() {
+    if (this.benefit < 50) {
+      this.benefit = this.benefit + 1;
+    }
+  }
+
+  updateBenefitWhenExpired() {
+    if (this.expiresIn < 0) {
+      this.benefit = this.benefit + 1;
+    }
+  }
+}
+
+class MagicPillTemplate extends DrugTemplate {
+  updateExpiresIn() {}
+  updateBenefit() {}
+  updateBenefitWhenExpired() {}
+}
+
+class FervexTemplate extends DrugTemplate {
+  updateBenefit() {
+    if (this.benefit === 50) return;
+    this.benefit = this.benefit + 1;
+    if (this.expiresIn < 10) {
+      this.benefit = this.benefit + 1;
+    }
+    if (this.expiresIn < 5) {
+      this.benefit = this.benefit + 1;
+    }
+  }
+
+  updateBenefitWhenExpired() {
+    if (this.expiresIn < 0) {
+      this.benefit = 0;
     }
   }
 }
