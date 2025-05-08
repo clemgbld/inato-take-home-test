4f91984592308053bd68d9a5976f161429f7b9f4
cgombauld <clement.gombauld@cpexterne.org>
2025-05-08 17:02:20 +0200

    refactor(Pharmacy): partion updateDaily in three step update expirationsIn , update benefit and additional update when expired

--- pharmacy.js
+++ pharmacy.js
@@ -6,20 +6,17 @@ export class Drug {
   }
 
   updateDaily() {
+    // handle expiration update
     if (this.name != "Magic Pill") {
       this.expiresIn = this.expiresIn - 1;
     }
+    // handle benefit update
     if (this.name === "Herbal Tea") {
       if (this.benefit < 50) {
         this.benefit = this.benefit + 1;
       }
-      if (this.expiresIn < 0) {
-        this.benefit = this.benefit + 1;
-      }
     } else if (this.name === "Fervex") {
-      if (this.expiresIn < 0) {
-        this.benefit = 0;
-      } else if (this.benefit < 50) {
+      if (this.benefit < 50) {
         this.benefit = this.benefit + 1;
         if (this.expiresIn < 10) {
           this.benefit = this.benefit + 1;
@@ -33,8 +30,15 @@ export class Drug {
       if (this.benefit > 0) {
         this.benefit = this.benefit - 1;
       }
+    }
 
-      if (this.expiresIn < 0) {
+    // handle benefit update when expires
+    if (this.expiresIn < 0) {
+      if (this.name === "Fervex") {
+        this.benefit = 0;
+      } else if (this.name === "Herbal Tea") {
+        this.benefit = this.benefit + 1;
+      } else {
         this.benefit = this.benefit - 1;
       }
     }
