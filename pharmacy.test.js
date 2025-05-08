e401561c86d406020325c7a63ac10eeb669e3007
cgombauld <clement.gombauld@cpexterne.org>
2025-05-08 15:12:51 +0200

    test(Pharmacy): add test that check that an Herbal Tea drug increase in benefit the older it gets

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -1,21 +1,31 @@
 import { Drug, Pharmacy } from "./pharmacy";
 
 describe("Pharmacy", () => {
-  it("should decrease the benefit and expiresIn", () => {
-    expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
-      [new Drug("test", 1, 2)],
-    );
+  describe("Standard drugs", () => {
+    it("should decrease the benefit and expiresIn", () => {
+      expect(
+        new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue(),
+      ).toEqual([new Drug("test", 1, 2)]);
+    });
+
+    it("should decrease the benefit twice as fast when expired", () => {
+      expect(
+        new Pharmacy([new Drug("test", 0, 3)]).updateBenefitValue(),
+      ).toEqual([new Drug("test", -1, 1)]);
+    });
+
+    it("should never decrase the benefit below 0", () => {
+      expect(
+        new Pharmacy([new Drug("test", 2, 0)]).updateBenefitValue(),
+      ).toEqual([new Drug("test", 1, 0)]);
+    });
   });
 
-  it("should decrease the benefit twice as fast when expired", () => {
-    expect(new Pharmacy([new Drug("test", 0, 3)]).updateBenefitValue()).toEqual(
-      [new Drug("test", -1, 1)],
-    );
-  });
-
-  it("should never decrase the benefit below 0", () => {
-    expect(new Pharmacy([new Drug("test", 2, 0)]).updateBenefitValue()).toEqual(
-      [new Drug("test", 1, 0)],
-    );
+  describe("Herbal Tea drug", () => {
+    it("should increase in benefit the older it gets", () => {
+      expect(
+        new Pharmacy([new Drug("Herbal Tea", 2, 3)]).updateBenefitValue(),
+      ).toEqual([new Drug("Herbal Tea", 1, 4)]);
+    });
   });
 });
